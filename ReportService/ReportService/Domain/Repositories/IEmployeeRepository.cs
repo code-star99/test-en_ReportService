f05f30d454412a0ac76fca6eb1070ce333b7da1e
Konrad <12345678+username@users.noreply.github.com>
2025-06-29 16:14:13 +0200

    feat(Domain): add repositories for employee and department management

---
+++ ReportService/ReportService/Domain/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReportService.Domain.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<string>> GetActiveDepartmentsAsync();
+        Task<List<Employee>> GetEmployeesByDepartmentAsync(string department);
+
+    }
+}
--- ReportService/ReportService/obj/Debug/netcoreapp2.0/ReportService.AssemblyInfo.cs
+++ ReportService/ReportService/obj/Debug/netcoreapp2.0/ReportService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae800bfea64e5be3c01fbc882d2632a3a08f91e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845fec885d224fee0c01fc00f26857b3fd29abc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
